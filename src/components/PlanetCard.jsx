c48c8cbec14917e3fe3d52a7efc515ae90808851
fabiano.justino <fibianojustino@gmail.com>
2022-02-18 18:26:41 -0300

    render a list of planets in the solar system

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropType from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card" className="planet-card">
-        <p data-testid="planet-name">{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName} ` } />
+        <p data-testid="planet-name">{ planetName }</p>
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system" className="solarSystem">
         <Title headline="Planetas" />
+
+        <section className="planet-container">
+          {planets.map((planet, index) => (
+            <PlanetCard
+              planetName={ planet.name }
+              planetImage={ planet.image }
+              key={ index }
+            />
+          ))}
+        </section>
       </div>
     );
   }
