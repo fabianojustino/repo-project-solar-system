68c9dd443374f95587fa47f1e64e57af20dfae68
fabiano.justino <fibianojustino@gmail.com>
2022-02-18 20:02:25 -0300

    fix requirement 5

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,7 +6,7 @@ class PlanetCard extends React.Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card" className="planet-card">
-        <img src={ planetImage } alt={ `Planeta ${planetName} ` } />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
         <p data-testid="planet-name">{ planetName }</p>
       </div>
     );
