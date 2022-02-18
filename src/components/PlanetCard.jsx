40a37a1b2c9bc888f046eb27b2dcf4b32925e39b
fabiano.justino <fibianojustino@gmail.com>
2022-02-18 17:10:38 -0300

    created a component called PlanetCard

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card" className="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName} ` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropType.string.isRequired,
+  planetImage: PropType.string.isRequired,
+};
+
+export default PlanetCard;
