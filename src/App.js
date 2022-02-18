783daa0e7ca3d69ab00247c44a8ea0491a35fe1a
fabiano.justino <fibianojustino@gmail.com>
2022-02-18 15:49:10 -0300

    create a component called Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
-  }
+    return (
+      <Header />
+    );
+  };
 }
 
 export default App;
