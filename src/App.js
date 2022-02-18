5327aa1585d79c06b86554a19c09904202bd6f83
fabiano.justino <fibianojustino@gmail.com>
2022-02-18 16:18:35 -0300

    create a component called SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <main>
+        <Header />
+        <SolarSystem />
+      </main>
     );
-  };
+  }
 }
 
 export default App;
